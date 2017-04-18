3f9516d7cf3edd3c4f9482e664e63eaf40a89642
chris <Chris@chrisdeMacBook-Pro.local>
2017-04-18 14:35:25 +0800

    自定义定位蓝点及测试MAUserLocationRepresentation属性

--- AMap.TEST/ViewController.m
+++ AMap.TEST/ViewController.m
@@ -13,21 +13,52 @@
 
 @interface ViewController ()
 
+/** mapView */
+@property (nonatomic, weak) MAMapView *mapView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     
-    ///初始化地图
-    MAMapView *_mapView = [[MAMapView alloc] initWithFrame:self.view.bounds];
+    //初始化地图
+    MAMapView *mapView = [[MAMapView alloc] initWithFrame:self.view.bounds];
+    _mapView = mapView;
     
-    ///把地图添加至view
-    [self.view addSubview:_mapView];
+    //把地图添加至view
+    [self.view addSubview:mapView];
+    
+    //地图显示时,自动定位到当前位置
+    _mapView.showsUserLocation = YES;
+    _mapView.userTrackingMode = MAUserTrackingModeFollow;
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    MAUserLocationRepresentation *locationRepresentation = [MAUserLocationRepresentation new];
+    //精度圈是否显示，默认YES
+    locationRepresentation.showsAccuracyRing = YES;
+    //是否显示方向指示(MAUserTrackingModeFollowWithHeading模式开启)。默认为YES
+    locationRepresentation.showsHeadingIndicator = YES;
+    //精度圈默认颜色
+    locationRepresentation.fillColor = kAccuracyCircleDefaultColor;
+    //精度圈 边线颜色, 默认 kAccuracyCircleDefaultColor
+    locationRepresentation.strokeColor = [UIColor purpleColor];
+    //精度圈 边线宽度，默认0
+    locationRepresentation.lineWidth = 2;
+    //内部蓝色圆点是否使用律动效果, 默认YES
+    locationRepresentation.enablePulseAnnimation = NO;
+    //定位点背景色，不设置默认白色
+    locationRepresentation.locationDotBgColor = [UIColor greenColor];
+    //定位点蓝色圆点颜色，不设置默认蓝色
+    locationRepresentation.locationDotFillColor = [UIColor grayColor];
+    //定位图标, 与蓝色原点互斥
+    locationRepresentation.image = [UIImage imageNamed:@"icon"];
+    
+    [_mapView updateUserLocationRepresentation:locationRepresentation];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
